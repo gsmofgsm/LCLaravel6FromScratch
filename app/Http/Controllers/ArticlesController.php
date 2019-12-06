4606e3882c2710b90aa34222c953812d680ec9ff
Qing Ma <qing.ma@isatis-health.nl>
2019-12-06 20:40:59 +0100

    the 7 restful controller actions

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -9,13 +9,42 @@ class ArticlesController extends Controller
 {
     public function index()
     {
+        // Show a list of resources
+
         $articles = Article::latest()->paginate(2);
         return view('articles.index', compact('articles'));
     }
 
     public function show($id)
     {
+        // Show a single resource
+
         $article = Article::findOrFail($id);
         return view('articles.show', compact('article'));
+    }
+
+    public function create()
+    {
+        // Show a view to create a new resource
+    }
+
+    public function store()
+    {
+        // Persist the new resource
+    }
+
+    public function edit()
+    {
+        // Show a view to edit an existing resource
+    }
+
+    public function update()
+    {
+        // Persist the edited resource
+    }
+
+    public function destroy()
+    {
+        // Delete the resource
     }
 }
